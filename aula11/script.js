196d484295cc69c3b193616dde03764f9896bcde
FeehZera <119156549+FeehZera@users.noreply.github.com>
2023-02-13 03:59:49 -0300

    a

--- aula11/script.js
+++ aula11/script.js
@@ -1,16 +1,3 @@
-var cor = document.getElementById('cor').value;
-var body = document.getElementById('body');
 
-
-function fundo() {
-    body.style.backgroundColor = cor
-    console.log(cor)
-}
-
-function reset() {
-    body.style.backgroundColor = ""
-    console.log(cor)
-    cor = ""
-}
 
 
